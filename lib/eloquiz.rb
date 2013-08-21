c9446d4749895304785bc1f5f9d522d26ebb3f37
Katrina Owen <katrina.owen@gmail.com>
2013-08-21 13:45:44 -0600

    Add hackday quiz question

--- lib/eloquiz.rb
+++ lib/eloquiz.rb
@@ -3,6 +3,7 @@ require './lib/eloquiz/option'
 require './lib/eloquiz/answer_key'
 require './lib/eloquiz/question'
 require './lib/eloquiz/question/conference_swag'
+require './lib/eloquiz/question/hackday'
 require './lib/eloquiz/question/business_meeting'
 require './lib/eloquiz/question/extracurricular_activities'
 require './lib/eloquiz/question/sports_team'
@@ -11,6 +12,7 @@ module Eloquiz
   def self.questions
     [
       ConferenceSwag.new,
+      Hackday.new(characters(8)),
       BusinessMeeting.new(characters(4)),
       ExtracurricularActivities.new(characters(4)),
       SportsTeam.new(characters(4))
