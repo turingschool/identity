3a2613c2a2e05cf47e20e9bda3124d5a2030437d
Katrina Owen <katrina.owen@gmail.com>
2013-08-21 14:42:20 -0600

    Add adoption quiz question

--- lib/eloquiz.rb
+++ lib/eloquiz.rb
@@ -3,6 +3,7 @@ require './lib/eloquiz/option'
 require './lib/eloquiz/answer_key'
 require './lib/eloquiz/question'
 require './lib/eloquiz/question/conference_swag'
+require './lib/eloquiz/question/adoption'
 require './lib/eloquiz/question/hackday'
 require './lib/eloquiz/question/business_meeting'
 require './lib/eloquiz/question/extracurricular_activities'
@@ -12,6 +13,7 @@ module Eloquiz
   def self.questions
     [
       ConferenceSwag.new,
+      Adoption.new(characters(9)),
       Hackday.new(characters(8)),
       BusinessMeeting.new(characters(4)),
       ExtracurricularActivities.new(characters(4)),
---
+++ lib/eloquiz/question/adoption.rb
@@ -0,0 +1,58 @@
+module Eloquiz
+  class Adoption < Question
+
+    def self.ages
+      (1..18).to_a.sample(7).sort
+    end
+
+    attr_reader :parent1, :parent2,
+      :a, :b, :c, :d, :e, :f, :g,
+      :y1, :y2, :y3, :y4, :y5, :y6, :y7
+    def initialize(players, ages = Adoption.ages)
+      @a, @b, @c, @d, @e, @f, @g, @parent1, @parent2 = *players
+      @y1, @y2, @y3, @y4, @y5, @y6, @y7 = *ages
+    end
+
+    def prompt
+      "#{parent1} and #{parent2} have adopted 7 children: #{a}, #{b}, #{c}, #{d}, #{e}, and #{f}. The children are #{y1}, #{y2}, #{y3}, #{y4}, #{y5}, #{y6}, and #{y7} years old."
+    end
+
+    def rules
+      [
+        "None of the other children come between #{a} and #{c} in age.",
+        "#{c} is older than #{e}",
+        "If #{e} is not #{y4} years old then #{d} is #{y4} years old.",
+        "At least one child is between the ages of #{f} and #{g}."
+      ]
+    end
+
+    def answers
+      [
+        ages(e, a, c, d, f, b, g),
+      ]
+    end
+
+    def red_herrings
+      [
+        ages(a, c, d, e, f, b, g),
+        ages(b, f, a, e, c, g, d),
+        ages(e, c, a, d, g, f, b),
+        ages(e, f, c, a, d, g, b)
+      ]
+    end
+
+    def type
+      :possible
+    end
+
+    private
+
+    def ages(*kids)
+      [y1, y2, y3, y4, y5, y6, y7].zip(kids).map do |age, kid|
+        "#{kid} is #{age}"
+      end.join(", ")
+    end
+
+  end
+end
+
