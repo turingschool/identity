6c05c6ae6a509d1f058ef0bf4e826c43d8c8e982
novohispano <novohispano@gmail.com>
2013-11-11 11:37:01 -0700

    Add dynamic progress bar.

--- app/helpers/applicant_helper.rb
+++ app/helpers/applicant_helper.rb
@@ -6,4 +6,10 @@ module ApplicantHelper
       url[/youtu.be\/([A-z|0-9|\-]*)/, 1]
     end
   end
+
+  def progress(user)
+    completed_steps = user.application.completed_steps.count
+    progress = (completed_steps / 5.to_f) * 100
+    progress.to_i
+  end
 end
