eb3d7c9103c1deafa69ad3ea643ffcc7c0bbc51b
novohispano <novohispano@gmail.com>
2013-11-19 11:42:40 -0700

    Design applicants index

--- app/controllers/admin/applicants_controller.rb
+++ app/controllers/admin/applicants_controller.rb
@@ -2,6 +2,7 @@ class Admin::ApplicantsController < AdminController
   def index
     @step = params[:step]
     @applications = Application.upto(@step)
+    @counts = Application.breakdown
   end
 
   def show
