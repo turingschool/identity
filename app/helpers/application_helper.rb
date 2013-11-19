54b752ddbd05230028b87a01dbbf929c4a52ec32
novohispano <novohispano@gmail.com>
2013-11-19 10:31:16 -0700

    Added total applicants and applicant per step info on dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def total_applicants(counts)
+    counts.each_value.collect.reduce(0, :+)
+  end
+
+  def applicants_per_step(counts, step)
+    (counts[step] / total_applicants(counts).to_f) * 100
+  end
 end
