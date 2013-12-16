17b94244da69cbea713795c5f31d70b921b151d1
novohispano <novohispano@gmail.com>
2013-12-16 11:30:22 -0700

    Add edit functionality on the welcome_applicant view

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,7 @@ class SiteController < ApplicationController
   def index
     unless current_user.guest?
       current_user.apply
+      @application = current_user.application
     end
   end
 end
