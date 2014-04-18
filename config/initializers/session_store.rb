e4fd719cbfb7d4f2906163e0ef4756243db8faa2
Josh Cheek <josh.cheek@gmail.com>
2014-04-18 10:24:34 -0600

    Share the session with other apps
    e.g. register for class
    https://github.com/JumpstartLab/register_for_class/blob/7f0909c837c4cd414efbd36d3ebf6c8deaaac8ee/config/initializers/session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Asquared::Application.config.session_store :cookie_store, key: '_asquared_session'
+Asquared::Application.config.session_store :cookie_store,
+  key:    '_jumpstartlab_session',
+  domain: :all
