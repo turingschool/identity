6bfa7e45737b12f7abd0fee81379cc3464b9de85
novohispano <novohispano@gmail.com>
2014-04-21 16:23:43 -0600

    Improve EvaluationController test

--- test/controllers/admin/evaluations_controller_test.rb
+++ test/controllers/admin/evaluations_controller_test.rb
@@ -4,23 +4,32 @@ class Admin::EvaluationsControllerTest < ActionController::TestCase
   attr_reader :user, :application
 
   def setup
-    @user = User.create!
+    @user = User.create! is_admin: true
     @controller.login user
     @application = user.apply!
   end
 
-  test "creates an InitialEvaluation" do
+  test 'creates an InitialEvaluation' do
     post :create_initial, id: user.id
     assert_response :redirect
+    evaluation = application.evaluations.first
+    assert_equal 'triage', evaluation.slug
+    assert_redirected_to edit_admin_evaluation_path(evaluation)
   end
 
-  test "creates an InterviewEvaluation" do
+  test 'creates an InterviewEvaluation' do
     post :create_interview, id: user.id
     assert_response :redirect
+    evaluation = application.evaluations.first
+    assert_equal 'selection', evaluation.slug
+    assert_redirected_to edit_admin_evaluation_path(evaluation)
   end
 
-  test "creates an LogicEvaluation" do
+  test 'creates a LogicEvaluation' do
     post :create_logic, id: user.id
     assert_response :redirect
+    evaluation = application.evaluations.first
+    assert_equal 'logic', evaluation.slug
+    assert_redirected_to edit_admin_evaluation_path(evaluation)
   end
 end
