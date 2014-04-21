da63c310f86f5f1fd6a675d89ea6dc320b8935ba
Josh Cheek <josh.cheek@gmail.com>
2014-04-21 11:33:24 -0600

    Redirect user back to invoking application after they authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ require './lib/github'
 class SessionsController < ApplicationController
 
   def new
+    session[:return_url] = params[:return_url] if params[:return_url]
     redirect_to Github.login_url
   end
 
@@ -31,7 +32,9 @@ class SessionsController < ApplicationController
       flash[:error] = "We're having trouble with logins right now. Please come back later."
     end
 
-    if current_user.admin?
+    if session[:return_url]
+      redirect_to session.delete(:return_url)
+    elsif current_user.admin?
       redirect_to admin_path
     else
       redirect_to root_path
