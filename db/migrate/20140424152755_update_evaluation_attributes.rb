b58f1b936b8b40d17dbbf269b0794a9f701681e5
Josh Cheek <josh.cheek@gmail.com>
2014-04-24 10:31:47 -0600

    Update migration so evaluations don't blow up when they are already changed

--- db/migrate/20140424152755_update_evaluation_attributes.rb
+++ db/migrate/20140424152755_update_evaluation_attributes.rb
@@ -3,21 +3,20 @@ class UpdateEvaluationAttributes < ActiveRecord::Migration
     Evaluation.all.each do |evaluation|
 
       evaluation.title = case evaluation.title
-                         when 'Initial Review'    then 'Initial Evaluation'
-                         when 'Interview Notes'   then 'Interview'
-                         when 'Logical Reasoning' then 'Logic Evaluation'
+                         when 'Initial Review',    'Initial Evaluation' then 'Initial Evaluation'
+                         when 'Interview Notes',   'Interview'          then 'Interview'
+                         when 'Logical Reasoning', 'Logic Evaluation'   then 'Logic Evaluation'
                          else
                            raise "Unexpected title: #{evaluation.title.inspect}"
                          end
 
       evaluation.slug =  case evaluation.slug
-                         when 'triage'    then 'initial_evaluation'
-                         when 'selection' then 'interview'
-                         when 'logic'     then 'logic_evaluation'
+                         when 'triage',    'initial_evaluation' then 'initial_evaluation'
+                         when 'selection', 'interview'          then 'interview'
+                         when 'logic',     'logic_evaluation'   then 'logic_evaluation'
                          else
                            raise "Unexpected slug: #{evaluation.slug.inspect}"
                          end
-
       evaluation.save!
     end
   end
