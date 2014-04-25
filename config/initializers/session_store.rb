680c28a84b641255d63f5e8197b1acdff6f674a4
Josh Cheek <josh.cheek@gmail.com>
2014-04-25 13:56:25 -0600

    Switch back to session from cookies
    Now that we are on Rails 4.1, they are sharing correctly across the services

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,3 +3,6 @@
 Asquared::Application.config.session_store :cookie_store,
   key:    '_jumpstartlab_session',
   domain: :all
+
+# to migrate sessions to use JSON (rails 4.1) instead of Marshall (rails 4.0)
+Rails.application.config.action_dispatch.cookies_serializer = :hybrid
