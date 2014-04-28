e5e78d15c538f67ecd3ffec1c83fae2ceb123ad9
Josh Cheek <josh.cheek@gmail.com>
2014-04-28 09:55:25 -0600

    Cookie name: _turing_session
    Change the cookie name from _jumpstartlab_session to _turing_session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Asquared::Application.config.session_store :cookie_store,
-  key:    '_jumpstartlab_session',
+  key:    '_turing_session',
   domain: :all
 
 # to migrate sessions to use JSON (rails 4.1) instead of Marshall (rails 4.0)
