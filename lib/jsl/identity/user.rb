1596c1a1f7e6718232738cdb90cc4b8225d5c652
Simon Taranto <simon.taranto@gmail.com>
2018-04-02 16:57:49 -0600

    Deprecate stripe_customer_id fields
    Why:
    
    * apply will no longer house this data shortly and enroll no longer
      needs it
    
    This change addresses the need by:
    
    * removing the `stripe_customer_id` field. This change will come with a
      major version bump since it's breaking.

--- lib/jsl/identity/user.rb
+++ lib/jsl/identity/user.rb
@@ -10,7 +10,6 @@ module Jsl
                     :github_id,
                     :avatar_url,
                     :gravatar_id,
-                    :stripe_customer_id,
                     :is_admin,
                     :is_invited,
                     :created_at,
